19cfad3ca9ecc145dcfc059598546d632ed6809d
Rey Lee <rlee@yondu.com>
2021-01-11 00:20:41 +0800

    DB configurations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
-    puts "=========== #{resource.admin?}"
     if resource.super_admin?
       events_path
     else
