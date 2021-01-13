77b62e48ef9f1f08eead8b4d6f073976a1174903
Rey Lee <rlee@yondu.com>
2021-01-14 02:39:14 +0800

    Added alert message for won players

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -90,7 +90,7 @@
   def draw_winner
     @guest_list = GuestList.find(params[:guest_list_id])
     if @guest_list.update_attributes winner_params
-      redirect_to event_path(@event)
+      redirect_to event_path(@event), notice: "#{@guest_list.user.full_name} won the raffle!"
     else
       render :draw_raffles
     end
