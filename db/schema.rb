8e7bf529ff99630f7c05e3a08b95c9ff669429b9
Rey Lee <rlee@yondu.com>
2021-01-20 11:36:13 +0800

    Cleaned up admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_131937) do
+ActiveRecord::Schema.define(version: 2021_01_20_031954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,20 +18,14 @@ ActiveRecord::Schema.define(version: 2021_01_10_131937) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "full_name"
     t.date "birthdate"
     t.string "contact_number"
-    t.integer "member_type", default: 0
-    t.string "member_id"
     t.string "affiliation"
     t.integer "role", default: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "events", force: :cascade do |t|
