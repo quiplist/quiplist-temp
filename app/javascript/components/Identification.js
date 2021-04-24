9ab8441cec2253c43ca551a062c0ed2209fd0a29
lee <lee.christinedazo@gmail.com>
2021-04-24 22:01:08 +0800

    fixed ui layout on menti

--- app/javascript/components/Identification.js
+++ app/javascript/components/Identification.js
@@ -19,19 +19,19 @@ class Identification extends React.Component {
 
     return (
       <div className="choices--identification">
-        <form className="row align-items-center mt-2"
+        <form className="row align-items-center mt-2 mb-2"
           onSubmit={e => {
             e.preventDefault()
             this.props.submitAnswer(this.state.answer)
           }}>
-          <div className="col-12 col-md-10 mb-2">
+          <div className="col-12 col-md-9">
             <div className="mb-0">
                 <input type="text" id="answer" name="answer" className="form-control" placeholder="Answer..." onChange={this.onChange} disabled={disabled}/>
             </div>
           </div>
 
-          <div className="col-12 col-md-2">
-            <button type="submit" className="btn btn-blue w-100" disabled={disabled}>Submit</button>
+          <div className="col-12 col-md-3 d-flex align-items-center justify-content-center">
+            <button type="submit" className="btn btn-blue" disabled={disabled}>Submit</button>
           </div>
         </form>
       </div>
